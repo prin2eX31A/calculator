5ad42f8ba1674b0814744ab4fbfa62fc28fd72b4
Prin2eX31A <115678771+prin2eX31A@users.noreply.github.com>
2024-01-26 10:43:20 +0800

    Content components

---
+++ index.js
@@ -0,0 +1,44 @@
+const  display = document.getElementById("display")
+
+let calString = ""
+function appendToDisplay(input){
+    if (display.value == "Error"){
+        display.value =""
+        calString =""
+    }
+    display.value += input;
+    calString += input
+}
+function clearDisplay(){
+    display.value = "";
+}
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
+function backSpace(){
+    let last = display.value.length;
+    display.value = display.value.slice(0,last-1);
+}
+
+function appendPercentToDisplay(){
+    let num = 0;
+    const reLastNumeric = /(\d+(\.\d*)?)$/;
+    //match \d = digits or \d+\.\d = decimal; ? with $ means the shortest answer starting from the end of the string
+
+    //using match function to get the number in string format
+    const numstr = display.value.match(reLastNumeric);
+
+    if (numstr){
+        //
+        num = parseFloat(numstr)*0.01;
+        display.value = display.value.replace(reLastNumeric, String(num));
+    }else{
+        display.value = 'Error';
+    }
+}
+
